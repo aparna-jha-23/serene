b75bc7549b209004a1e58ecc70d30f36d4b2c46c
aparna-jha-23 <121332698+aparna-jha-23@users.noreply.github.com>
2023-03-30 17:50:22 +0530

    added depression.js

---
+++ steps/src/pages/depression.js
@@ -0,0 +1,99 @@
+import "./depression.css";
+
+const Depression = () => {
+    return (
+        
+        <div className="page">
+            
+            <div className="helpline">
+                <div className="depressbox">
+                    <h1>HELP IS AVAILABLE TALK TO SOMEONE TODAY</h1>
+                    <p>Remember, suicide is a permanent solution to a temporary problem. There is help available, and with the right support, you can overcome your struggles and find a brighter future.</p><br></br>
+                    <button className="contactbtn" onClick="window.location.href='https://www.practo.com/delhi/psychiatrist';">CONTACT YOUR NEAREST PROFFESIONAL</button><br></br><br></br><br></br><br></br>
+                    <h3>HELPLINES IN INDIA</h3>
+                    <div className="helps">
+                        <div className="helpbox">
+                            <h5 className="heading" >NIMHANS Psychosocial Support Helpline</h5><br></br>
+                            <p className="info">Open 24/7<br></br><br></br>
+                            Counselors<br></br><br></br>
+                            080-46110007</p><br></br>
+                            <a href="https://nimhans.ac.in/" target="_blank">nimhans.ac.in</a>
+
+                        </div>
+                        <div className="helpbox">
+                        <h5 className="heading" >NIMHANS Psychosocial Support Helpline</h5><br></br>
+                            <p className="info">Open 24/7<br></br><br></br>
+                            Counselors<br></br><br></br>
+                            080-46110007</p><br></br>
+                            <a href="https://nimhans.ac.in/" target="_blank">nimhans.ac.in</a>
+                            
+                        </div>
+                        <div className="helpbox">
+                        <h5 className="heading" >NIMHANS Psychosocial Support Helpline</h5><br></br>
+                            <p className="info">Open 24/7<br></br><br></br>
+                            Counselors<br></br><br></br>
+                            080-46110007</p><br></br>
+                            <a href="https://nimhans.ac.in/" target="_blank">nimhans.ac.in</a>
+                            
+                        </div>
+                        <div className="helpbox">
+                        <h5 className="heading" >NIMHANS Psychosocial Support Helpline</h5><br></br>
+                            <p className="info">Open 24/7<br></br><br></br>
+                            Counselors<br></br><br></br>
+                            080-46110007</p><br></br>
+                            <a href="https://nimhans.ac.in/" target="_blank">nimhans.ac.in</a>
+                            
+                        </div>
+                        <div className="helpbox">
+                        <h5 className="heading" >NIMHANS Psychosocial Support Helpline</h5><br></br>
+                            <p className="info">Open 24/7<br></br><br></br>
+                            Counselors<br></br><br></br>
+                            080-46110007</p><br></br>
+                            <a href="https://nimhans.ac.in/" target="_blank">nimhans.ac.in</a>
+                            
+                        </div>
+                        <div className="helpbox">
+                        <h5 className="heading" >NIMHANS Psychosocial Support Helpline</h5><br></br>
+                            <p className="info">Open 24/7<br></br><br></br>
+                            Counselors<br></br><br></br>
+                            080-46110007</p><br></br>
+                            <a href="https://nimhans.ac.in/" target="_blank">nimhans.ac.in</a>
+                            
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div className="remember">
+                <div className="rememberbox">
+                    <br></br><br></br>
+                    <h3>REMEMBER</h3>
+                    <br></br>
+                    <p>
+          Breaking Isolation: Depression can make you feel isolated and alone, but talking to someone can help you break out of that cycle of isolation and loneliness. When you reach out to others, you can start to build a supportive network of friends and family members who care about you.
+<br/><br/>
+Getting Support: When you reach out to others, you can receive emotional support and encouragement. This can help you feel less alone and more hopeful about the future. People who are supportive can also help you make a plan to get the help you need to manage your depression.
+<br/><br/>
+Improving Your Mental Health: Talking about your feelings with a trusted friend or family member, or a mental health professional, can help you better understand your depression and develop a plan to manage it. With the right support and treatment, you can begin to feel better and overcome your depression.
+<br/><br/>
+Reducing Stigma: Depression is a common and treatable mental health condition, but it is often stigmatized. Reaching out to others can help reduce the stigma associated with depression and promote a more open and understanding society.
+<br/><br/>
+Encouraging Self-Care: When you reach out to others, you can receive practical advice and support to help you take care of yourself. This may include tips for eating well, getting enough sleep, and engaging in physical activity
+          </p>
+          <div class="spacer"></div>
+                </div>
+
+
+            </div>
+
+
+        </div>
+
+
+      
+    );
+  };
+  
+  export default Depression;
